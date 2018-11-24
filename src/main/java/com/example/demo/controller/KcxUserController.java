bf03805aceb5d93a6dd96cdbf1ff75586951d62f
DESKTOP-RRV5BLJ\qukoucai001 <2929094294@qq.com>
2018-11-24 13:46:00 +0800

    KcxUserController

---
+++ src/main/java/com/example/demo/controller/KcxUserController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import java.util.Map;
+
+/**
+ * Created by qukoucai001 on 2018/11/24.
+ */
+
+
+
+public class KcxUserController {
+    public static void main(String[] args) {
+        System.out.println("KcxUserController");
+    }
+
+}
